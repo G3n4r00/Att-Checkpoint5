f2ac257e11b26d90901279563304cce1a9fa1a72
G3n4r00 <rm551986@fiap.com.br>
2023-10-01 23:49:59 -0300

    comentários

--- src/routes/AddProduto.jsx
+++ src/routes/AddProduto.jsx
@@ -6,24 +6,31 @@ import { useNavigate } from 'react-router-dom';
 export default function AddProduto() {
   document.title = "ADICIONAR PRODUTO";
   
+  // Inicializa o hook useState para controlar o estado do novo produto
   const navigate = useNavigate();
   const [novoProduto, setNovoProduto] = useState({
-    nome: '',
-    desc: '',
-    preco: 0,
+    nome: '', // Inicializa o nome do novo produto como uma string vazia
+    desc: '', // Inicializa a descrição do novo produto como uma string vazia
+    preco: 0, // Inicializa o preço do novo produto como 0
   });
 
+   // Função para lidar com mudanças nos campos de input
   const handleChange = (e) => {
     const { name, value } = e.target;
+    // Atualiza o estado do novo produto com os valores dos campos de input usando spread operator
     setNovoProduto({ ...novoProduto, [name]: value });
   };
 
+  // Função para lidar com o envio do formulário
   const handleSubmit = (e) => {
+    // Impede o comportamento padrão do formulário de recarregar a página
     e.preventDefault();
+    // Cria um novo objeto de produto com um ID único baseado no comprimento da lista de produtos existente
     const novoProdutoComId = {
-      ...novoProduto,
-      id: ListaProdutos.length + 1,
+      ...novoProduto, // Mantém as propriedades existentes do novo produto
+      id: ListaProdutos.length + 1, // Adiciona uma nova propriedade "id" ao novo produto com um valor único
     };
+    // Adiciona o novo produto à lista de produtos existente e alerta o usuário
     ListaProdutos.push(novoProdutoComId);
     alert('Produto adicionado com SUCESSO!');
     navigate('/produtos');
